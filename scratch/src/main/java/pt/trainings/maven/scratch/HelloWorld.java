90688192f42ff2d82c5e3d4c9809c5c24bde2ef8
MykolaMedynskyi <mykola.medynskyi@gmail.com>
2018-10-23 14:12:06 +0200

    first commit

---
+++ scratch/src/main/java/pt/trainings/maven/scratch/HelloWorld.java
@@ -0,0 +1,15 @@
+package pt.trainings.maven.scratch;
+
+/**
+ * @author bianero
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Greetings greetings = new Greetings();
+		System.out.println(greetings.sayHello(args.length>0?args[0]:null));
+		System.out.println(greetings.sayHello("Mykola"));
+	}
+
+}
